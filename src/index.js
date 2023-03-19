32ceb8f3f01f4732572c8e185917f4efac82fcec
NataliaJeszke <nataliajeszke8@gmail.com>
2023-03-19 20:36:04 +0100

    redux and store instal and config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import indexStyle from './index.css'
+import indexStyle from "./index.css";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { store } from "./store/index";
+
 // import { Provider } from "react-redux";
 // import { store } from "./store/index";
 // import { persistStore } from "redux-persist";
@@ -11,6 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // let persistor = persistStore(store)
 root.render(
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>
 );

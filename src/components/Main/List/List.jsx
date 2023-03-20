a6701d2d8325275a889fd56ef2371ba4d23ac097
NataliaJeszke <nataliajeszke8@gmail.com>
2023-03-20 22:37:27 +0100

    problem with tbody that got div - error, solved

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,13 +2,9 @@ import { ListItem } from "../../ListItem/ListItem";
 export function List({ items }) {
   return (
     <div>
-      <table>
-        <tbody>
           {items.map((item, index) => (
             <ListItem key={item + index} item={item} />
           ))}
-        </tbody>
-      </table>
     </div>
   );
 }

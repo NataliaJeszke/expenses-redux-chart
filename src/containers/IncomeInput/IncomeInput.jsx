d30258c3978af87a2fec19c3ba2a1bd1deca19ca
NataliaJeszke <nataliajeszke8@gmail.com>
2023-03-23 13:17:32 +0100

    Income input error removed, changed onChange function

--- src/containers/IncomeInput/IncomeInput.jsx
+++ src/containers/IncomeInput/IncomeInput.jsx
@@ -1,21 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setIncomeAction } from "../../store/income/income-slice";
 import incomeStyle from "./IncomeInputStyle.module.css";
+
 export function IncomeInput(props) {
   const income = useSelector((store) => store.INCOME.income);
+
   const dispatch = useDispatch();
+
   function setIncome(event) {
     dispatch(setIncomeAction(Number.parseFloat(event.target.value)));
   }
+
   return (
     <form className={incomeStyle.incomeForm}>
-      <label className={incomeStyle.incomeLabel} htmlFor="income">Add your income:</label>
+      <label className={incomeStyle.incomeLabel} htmlFor="income">
+        Add your income:
+      </label>
       <input
-      className={incomeStyle.incomeInput}
+        className={incomeStyle.incomeInput}
         type="number"
         id="income"
         defaultValue={income}
-        onChange={(event) => setIncome(event.target.value)}
+        onChange={setIncome}
       />
     </form>
   );

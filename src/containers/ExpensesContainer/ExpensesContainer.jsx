8efa7647e411d6215cde21ce6aeb05efb68e1f87
NataliaJeszke <nataliajeszke8@gmail.com>
2023-03-29 21:46:25 +0200

    Filter selected categories as a component file

--- src/containers/ExpensesContainer/ExpensesContainer.jsx
+++ src/containers/ExpensesContainer/ExpensesContainer.jsx
@@ -1,11 +1,9 @@
 import { List } from "../../components/Main/List/List";
-import { FilterButtons } from "../../components/Main/FilterButtons/FilterButtons";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { ExpenseTotal } from "../ExpenseTotal/ExpenseTotal";
 import ExpensesContainerStyle from "./ExpensesContainerStyle.module.css";
-import Select, { StylesConfig } from "react-select";
-import catalogue from "../SelectInput/catalogue";
+import FilterSelect from "../FilterSelect/FilterSelect";
 
 export function ExpensesContainer({}) {
   //Get all items (expenses) from store and filter by categories//
@@ -29,17 +27,9 @@ export function ExpensesContainer({}) {
 
   return (
     <div className={ExpensesContainerStyle.container}>
-      <Select
-        closeMenuOnSelect={false}
-        isMulti
-        options={catalogue}
-        onChange={handleCategoryClick}
-        value={selectedCategories.map((category) => ({
-          value: category,
-          label: category,
-        }))}
-        className="basic-multi-select"
-        classNamePrefix="select"
+      <FilterSelect
+        selectedCategories={selectedCategories}
+        handleCategoryClick={handleCategoryClick}
       />
       <List items={filteredExpenses} />
       <ExpenseTotal expenses={filteredExpenses} />
--- src/containers/FilterSelect/FilterSelect.jsx
+++ src/containers/FilterSelect/FilterSelect.jsx
@@ -1,61 +1,21 @@
-import React from "react";
-
-import Select, { StylesConfig } from "react-select";
+import Select from "react-select";
 import catalogue from "../SelectInput/catalogue";
 
-// const colourStyles: StylesConfig <catalogue, true> = {
-//   control: (styles) => ({ ...styles, backgroundColor: 'white' }),
-//   option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-//     const color = chroma(data.color);
-//     return {
-//       ...styles,
-//       backgroundColor: isDisabled
-//         ? undefined
-//         : isSelected
-//         ? data.color
-//         : isFocused
-//         ? color.alpha(0.1).css()
-//         : undefined,
-//       color: isDisabled
-//         ? '#ccc'
-//         : isSelected
-//         ? chroma.contrast(color, 'white') > 2
-//           ? 'white'
-//           : 'black'
-//         : data.color,
-//       cursor: isDisabled ? 'not-allowed' : 'default',
+const FilterSelect = ({ selectedCategories, handleCategoryClick }) => {
+  return (
+    <Select
+      closeMenuOnSelect={false}
+      isMulti
+      options={catalogue}
+      onChange={handleCategoryClick}
+      value={selectedCategories.map((category) => ({
+        value: category,
+        label: category,
+      }))}
+      className="basic-multi-select"
+      classNamePrefix="select"
+    />
+  );
+};
 
-//       ':active': {
-//         ...styles[':active'],
-//         backgroundColor: !isDisabled
-//           ? isSelected
-//             ? data.color
-//             : color.alpha(0.3).css()
-//           : undefined,
-//       },
-//     };
-//   },
-//   multiValue: (styles, { data }) => {
-//     const color = chroma(data.color);
-//     return {
-//       ...styles,
-//       backgroundColor: color.alpha(0.1).css(),
-//     };
-//   },
-//   multiValueLabel: (styles, { data }) => ({
-//     ...styles,
-//     color: data.color,
-//   }),
-//   multiValueRemove: (styles, { data }) => ({
-//     ...styles,
-//     color: data.color,
-//     ':hover': {
-//       backgroundColor: data.color,
-//       color: 'white',
-//     },
-//   }),
-// };
-
-export default (onCategoryClick) => (
-  <Select closeMenuOnSelect={false} isMulti options={catalogue} onClick={() => onCategoryClick()} />
-);
+export default FilterSelect;
